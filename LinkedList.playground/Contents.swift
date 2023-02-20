465d2bd51c86253b4f95a3aea7a385a3afbc16b4
vinhhoang <dr.vinhhoang@gmail.com>
2023-02-20 07:55:06 +0700

    update linked list

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -64,18 +64,117 @@
 //    print("Removed value: " + String(describing: removedValue))
 //}
 
-example(of: "removing a node after a particular node") {
+//example(of: "removing a node after a particular node") {
+//    var list = LinkedList<Int>()
+//    list.push(3)
+//    list.push(2)
+//    list.push(1)
+//
+//    print("Before removing at particular index: \(list)")
+//    let index = 0
+//    let node = list.node(at: index)!
+//    let removedValue = list.remove(after: node)
+//    print("After removing at index \(index): \(list)")
+//    print("Removed value: " + String(describing: removedValue))
+//
+//}
+
+//example(of: "using collection") {
+//    var list = LinkedList<Int>()
+//    for i in 0...9 {
+//        list.append(i)
+//    }
+//
+//    print("List: \(list)")
+//    print("First element: \(list[list.startIndex])")
+//    print("Array containing first 3 elements: \(Array(list.prefix(3)))")
+//    print("Array containing last 3 elements: \(Array(list.suffix(3)))")
+//
+//    let sum = list.reduce(0, +)
+//    print("Sum of all values: \(sum)")
+//}
+
+//example(of: "array copy on write") {
+//    let array1 = [1, 2]
+//    var array2 = array1
+//
+//    print("Array1: \(array1)")
+//    print("Array2: \(array2)")
+//
+//    print("---After adding 3 to array 2---")
+//    array2.append(3)
+//    print("Array1: \(array1)")
+//    print("Array2: \(array2)")
+//
+//}
+
+
+//example(of: "linked list cow") {
+//    var list1 = LinkedList<Int>()
+//    list1.append(1)
+//    list1.append(2)
+//    var list2 = list1
+//    print("List1: \(list1)")
+//    print("List2: \(list2)")
+//    print("After appending 3 to list 2")
+//    list2.append(3)
+//    print("List1: \(list1)")
+//    print("List2: \(list2)")
+//
+//    print("Removing middle node on list 2")
+//    if let node = list2.node(at: 0) {
+//        list2.remove(after: node)
+//    }
+//    print("List2: \(list2)")
+//}
+
+//example(of: "print in reverse") {
+//    var list = LinkedList<Int>()
+//    list.push(3)
+//    list.push(2)
+//    list.push(1)
+//    print(list)
+//    var reverseList = list
+//    while let last = reverseList.removeLast() {
+//        print(last)
+//    }
+//    print(list)
+//}
+
+//example(of: "find the middle node") {
+//    var list = LinkedList<Int>()
+//   // list.push(4)
+//    list.push(3)
+//    list.push(2)
+//    list.push(1)
+//
+//    var count = 0
+//    var currentNode = list.head
+//    if currentNode != nil {
+//        count = 1
+//    }
+//    while let next = currentNode?.next {
+//        currentNode = next
+//        count += 1
+//    }
+//    let midleIndex = count / 2
+//    print(midleIndex)
+//    let node = list.node(at: midleIndex)
+//    print(node?.value)
+//
+//}
+
+example(of: "reverse linked list") {
     var list = LinkedList<Int>()
-    list.push(3)
-    list.push(2)
-    list.push(1)
+      list.push(4)
+      list.push(3)
+      list.push(2)
+      list.push(1)
+        
+    list.reverse()
     
-    print("Before removing at particular index: \(list)")
-    let index = 0
-    let node = list.node(at: index)!
-    let removedValue = list.remove(after: node)
-    print("After removing at index \(index): \(list)")
-    print("Removed value: " + String(describing: removedValue))
+    print(list)
     
 }
+
 
--- LinkedList.playground/Sources/LinkedList.swift.swift
+++ LinkedList.playground/Sources/LinkedList.swift.swift
@@ -11,6 +11,7 @@ public struct LinkedList<Value> {
     }
     
     public mutating func push(_ value: Value) {
+        copyNodes()
         head = Node(value: value, next: head)
         if tail == nil {
             tail = head
@@ -18,6 +19,7 @@ public struct LinkedList<Value> {
     }
     
     public mutating func append(_ value: Value) {
+        copyNodes()
         guard !isEmpty else {
             push(value)
             return
@@ -42,6 +44,7 @@ public struct LinkedList<Value> {
     public mutating func insert(_ value: Value,
                                 after node: Node<Value>)
     -> Node<Value> {
+        copyNodes()
         guard tail !== node else {
             append(value)
             return tail!
@@ -53,6 +56,7 @@ public struct LinkedList<Value> {
     
     @discardableResult
     public mutating func pop() -> Value? {
+        copyNodes()
         defer {
             head = head?.next
             if isEmpty {
@@ -63,6 +67,7 @@ public struct LinkedList<Value> {
     }
     
     public mutating func removeLast() -> Value? {
+        copyNodes()
         guard let head = head else {
             return nil
         }
@@ -85,6 +90,9 @@ public struct LinkedList<Value> {
     }
     
     public mutating func remove(after node: Node<Value>) -> Value? {
+        guard let node = copyNodes(returningCopyOf: node) else {
+            return nil
+        }
         defer {
             if node.next === tail {
                 tail = node
@@ -94,6 +102,110 @@ public struct LinkedList<Value> {
         return node.next?.value
     }
     
+    public mutating func reverse() {
+        tail = head
+        var prev = head
+        var current = head?.next
+        prev?.next = nil
+        
+        while current != nil {
+            let next = current?.next
+            current?.next = prev
+            prev = current
+            current = next
+        }
+        head = prev
+    }
+    
+    
+    // MARK: - PRIVATE
+    private mutating func copyNodes() {
+        guard !isKnownUniquelyReferenced(&head) else {
+            return
+        }
+        guard var oldNode = head else {
+            return
+        }
+        head = Node(value: oldNode.value)
+        var newNode = head
+        while let nextOldNode = oldNode.next {
+            newNode!.next = Node(value: nextOldNode.value)
+            newNode = newNode!.next
+            
+            oldNode = nextOldNode
+        }
+        
+        tail = newNode
+    }
+    
+    private mutating func copyNodes(returningCopyOf node: Node<Value>?) -> Node<Value>? {
+        guard !isKnownUniquelyReferenced(&head) else {
+            return nil
+        }
+        guard var oldNode = head else {
+            return nil
+        }
+        
+        head = Node(value: oldNode.value)
+        var newNode = head
+        var nodeCopy: Node<Value>?
+        
+        while let nextOldNode = oldNode.next {
+            if oldNode === node {
+                nodeCopy = newNode
+            }
+            newNode!.next = Node(value: nextOldNode.value)
+            newNode = newNode!.next
+            oldNode = nextOldNode
+        }
+        return nodeCopy
+    }
+    
+}
+
+extension LinkedList: Collection {
+
+    public struct Index: Comparable {
+        public var node: Node<Value>?
+
+        static public func ==(lhs: Index, rhs: Index) -> Bool {
+            switch (lhs.node, rhs.node) {
+            case let (left?, right?):
+                return left.next === right.next
+            case (nil, nil):
+                return true
+            default:
+                return false
+            }
+        }
+
+        static public func <(lhs: Index, rhs: Index) -> Bool {
+            guard lhs != rhs else {
+                return false
+            }
+            let nodes = sequence(first: lhs.node, next: { $0?.next })
+            return nodes.contains { $0 === rhs.node }
+        }
+    }
+
+    public var startIndex: Index {
+        Index(node: head)
+    }
+
+    public var endIndex: Index {
+        Index(node: tail?.next)
+    }
+
+    public func index(after i: Index) -> Index {
+        Index(node: i.node?.next)
+    }
+
+    public subscript(position: Index) -> Value {
+        position.node!.value
+    }
+
+
+
 }
 
 extension LinkedList: CustomStringConvertible {
